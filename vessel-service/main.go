eabd8e5f3b6b5c6bca05bea56dbe1d81785ff014
goss.beta <goss.beta@gmail.com>
2019-01-06 14:21:25 +0900

    update vessel service

--- vessel-service/main.go
+++ vessel-service/main.go
@@ -23,6 +23,9 @@ func main() {
 		log.Fatalf("create session error: %v", err)
 	}
 
+	repo := &VesselRepository{session.Copy()}
+	CreateDummyData(repo)
+
 	server := micro.NewService(
 		micro.Name("go.micro.srv.vessel"),
 		micro.Version("latest"),
@@ -33,5 +36,18 @@ func main() {
 
 	if err := server.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func CreateDummyData(repo Repository) {
+	defer repo.Close()
+	vessels := []*pb.Vessel{
+		{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	}
+	for _, v := range vessels {
+		err := repo.Create(v)
+		if err != nil {
+			log.Fatalf("failed to create vessel: %v", err)
+		}
 	}
 }
